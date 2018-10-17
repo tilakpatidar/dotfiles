319796b2bbcf48971763c8e149275c00aab0af60
tilakpatidar <tilakpatidar@gmail.com>
2018-10-17 19:04:02 +0530

    Add zsh autosuggestions and vundle

--- install-zsh-plugins.sh
+++ install-zsh-plugins.sh
@@ -5,3 +5,7 @@ echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> 
 cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins
 git clone https://github.com/djui/alias-tips.git
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
